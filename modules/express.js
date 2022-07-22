282c37647a2841ec707282374ea845220287d3b1
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-22 19:59:49 -0300

    middlewares

--- modules/express.js
+++ modules/express.js
@@ -3,20 +3,103 @@ const UserModel = require('../src/models/user.model');
 const app = express();
 
 const port = 8080;
+app.use(express.json());
 
-app.get('/home', (req, res) => {
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    console.log(`Content-Type: ${req.headers['Content-Type']}`);
+    console.log(`Content-Length: ${req.headers['Content-Length']}`)
+    console.log(`Date: ${new Date()}`);
+
+    next();
+})
+
+/*app.get('/home', (req, res) => {
     res.contentType('application/html');
     res.status(200).send('<h1>Hello World</h1>');
 });
 app.get('/users', (req, res) => {
     res.contentType('application/json');
     res.status(200).send(JSON.stringify([{name: 'Felipe'}, {name: 'João'}]));
-});
-app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}/home`);
+});*/
+
+app.get('/users', (req, res) => {
+    try{
+        UserModel.find({}, (err, users) => {
+            if(err){
+                res.status(500).send(err);
+            }else{
+                res.status(200).send(users);
+            }
+        });
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
 });
 
+app.get('/users/:id', async (req, res) => {
+    try{
+        const user = await UserModel.findById(req.params.id);
+        if(!user){
+            res.status(404).send('User not found');
+        }else{
+            res.status(200).send(user);
+        }
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}); // async/await
+
+
 app.post('/users', async (req, res) => {
-    const user = await new UserModel(req.body);
-    res.status(201).json(user);
+    try{
+        const user = new UserModel(req.body);
+        await user.save();
+        res.status(201).send(user);
+    }catch(err){
+        res.status(500).send(err);
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    try{
+        const user = await UserModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!user){
+            res.status(404).send('User not found');
+        }else{
+            res.status(200).send(user);
+        
+        }
+    }catch(err){
+        res.status(500).send(err);
+    }
+});
+
+/*app.patch('users/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+    res.status(200).send(user);
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+
+});*/
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await UserModel.findByIdAndDelete(req.params.id);
+        if(!user){
+            res.status(404).send('User not found');
+        }else{
+            res.status(200).send(user);
+        }
+    }catch(err){
+        res.status(500).send(err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running`);
 });
